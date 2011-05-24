f0f363965ff14f6c10e4e94a9dac3cd3c650a980
fallenangelX <iceis2@hotmail.com>
2011-05-24 14:32:15 -0400

    (X:47} little addition for sd2 script Swp

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -12284,6 +12284,11 @@ SpellAuraHolder* Unit::GetSpellAuraHolder (uint32 spellid, ObjectGuid casterGuid
     return NULL;
 }
 
+void Unit::RemoveUnitFromHostileRefManager(Unit* p_unit) 
+{ 
+    getHostileRefManager().deleteReference(p_unit); 
+}
+
 void Unit::_AddAura(uint32 spellID, uint32 duration)
 {
     SpellEntry const *spellInfo = sSpellStore.LookupEntry( spellID );
