d8b9acf7a89822849b0d72aa731c08a8189e7687
fallenangelX <iceis2@hotmail.com>
2011-05-24 13:06:27 -0400

    {X:36} as of 3.3.3  HK have had a 100% increase to honor

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -6983,6 +6983,7 @@ bool Player::RewardHonor(Unit *uVictim, uint32 groupsize, float honor)
             honor /= groupsize;
 
         honor *= (((float)urand(8,12))/10);                 // approx honor: 80% - 120% of real honor
+        honor *= 2.0f;
     }
 
     // honor - for show honor points in log
