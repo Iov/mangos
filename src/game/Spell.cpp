d35df5a473dcd26a5645d524195bc4b7769adad5
Ankso <administrador.ankso@reinodelaoscuridad.com>
2011-05-25 14:56:25 +0200

    [IOV326] Player with Cyclone can only use PvP trinket or Every Man For Himself
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -6524,6 +6524,10 @@ SpellCastResult Spell::CheckCasterAuras() const
             mechanic_immune = IMMUNE_TO_MOVEMENT_IMPAIRMENT_AND_LOSS_CONTROL_MASK;
     }
 
+    // Player with Cyclone can only use PvP trinket or Every Man For Himself
+    if (m_caster->HasAura(33786) && mechanic_immune != IMMUNE_TO_MOVEMENT_IMPAIRMENT_AND_LOSS_CONTROL_MASK)
+        return SPELL_FAILED_STUNNED;
+
     // Check whether the cast should be prevented by any state you might have.
     SpellCastResult prevented_reason = SPELL_CAST_OK;
     // Have to check if there is a stun aura. Otherwise will have problems with ghost aura apply while logging out
