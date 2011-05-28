8c822aee1d80d23f3438fbcacc031572ee5989c8
Feanordev <Feanordev@gmail.com>
2011-05-28 20:56:57 +0200

    [IOV355] Added Divine Hymn heal bonus
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -4527,6 +4527,10 @@ void Spell::EffectHeal(SpellEffectIndex /*eff_idx*/)
             }
         }
 
+        // Divine Hymn
+        if(m_spellInfo->Id == 64844)
+            addhealth += caster->SpellBaseHealingBonusDone(GetSpellSchoolMask(m_spellInfo)) * m_spellInfo->EffectCoeffs[0];
+
         addhealth = caster->SpellHealingBonusDone(unitTarget, m_spellInfo, addhealth, HEAL);
         addhealth = unitTarget->SpellHealingBonusTaken(caster, m_spellInfo, addhealth, HEAL);
 
