1284575973d39f20f5d9c648e3f7e0d9985f8458
Janu <jaed1@gmx.net>
2011-06-03 09:13:29 +0200

    [IOV387] Restore build after last merge
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -4378,11 +4378,7 @@ bool ChatHandler::HandleCharacterLevelCommand(char* args)
         newlevel = STRONG_MAX_LEVEL;
 
     if (newlevel > int32(sWorld.getConfig(CONFIG_UINT32_MAX_PLAYER_LEVEL)))
-<<<<<<< HEAD
-        newlevel = sWorld.getConfig(CONFIG_UINT32_MAX_PLAYER_LEVEL);
-=======
         newlevel = int32(sWorld.getConfig(CONFIG_UINT32_MAX_PLAYER_LEVEL));
->>>>>>> mr2/master
 
     HandleCharacterLevel(target, target_guid, oldlevel, newlevel);
 
@@ -4430,11 +4426,7 @@ bool ChatHandler::HandleLevelUpCommand(char* args)
         newlevel = STRONG_MAX_LEVEL;
 
     if (newlevel > int32(sWorld.getConfig(CONFIG_UINT32_MAX_PLAYER_LEVEL)))
-<<<<<<< HEAD
-        newlevel = sWorld.getConfig(CONFIG_UINT32_MAX_PLAYER_LEVEL);
-=======
         newlevel = int32(sWorld.getConfig(CONFIG_UINT32_MAX_PLAYER_LEVEL));
->>>>>>> mr2/master
 
     HandleCharacterLevel(target, target_guid, oldlevel, newlevel);
 
